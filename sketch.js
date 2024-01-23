c4fb40b855599a31f759e89a847e7da9ca5e9f71
Fellstrike <nickwihtol@gmail.com>
2024-01-23 11:06:14 -0800

    Initial Commit

--- sketch.js
+++ sketch.js
@@ -5,7 +5,46 @@ function setup()
 
 function draw() 
 {
-  background(0);
-  fill(200, 100, 255);
-  ellipse(mouseX, mouseY, 200, 100);
+  background(200, 155, 255);
+
+  let size = 750;
+  let red = 200;
+  let green = 155;
+  let blue = 255;
+  let fillColor = color(red, green, blue);
+  let xPos = 400;
+  let yPos = 10;
+  let count = 0;
+
+  noStroke();
+  noFill();
+  drawShape(xPos - 30, yPos + 50, color(red - 50, green - 30, blue - 60), size - 40);
+  stroke(155);
+  while (size >= 0)
+  {
+    drawShape(xPos, yPos, fillColor, size);
+    size = size - 5;
+    red --;
+    green -= 2
+    if (count >= 1)
+    {
+      blue--;
+      count = 0;
+    }
+    else
+    {
+      count++;
+    }
+    fillColor = color(red, green, blue);
+    stroke(red +10, green + 20, blue + 5);
+  }
+}
+
+function drawShape(x, y, fillColor, size) 
+{
+  fill(fillColor);
+  beginShape();
+  vertex(x, y);
+  bezierVertex(x + size , y + size, x - size, y + size, x, y);
+  endShape(CLOSE);
 }
